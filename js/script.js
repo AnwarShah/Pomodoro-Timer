402a79598ba906d2e6078a3c94b7fd9d0dc601f2
Mohammad Anwar Shah <mohammadanwarshah@gmail.com>
2016-06-25 15:28:31 +0600

    Disable timer control while running

--- js/script.js
+++ js/script.js
@@ -1,12 +1,18 @@
+var timerRunning = false; // timer state
+
 function updateTime(time, timerDisplay) {
   timerDisplay.text(time);
 }
 
 function countDown(time, timerDisplay) {
   var time = time;
+  timerRunning = true; // set the flag
   var timeoutId = setInterval(function(){
     time = time - 1;
-    if( time <= 0 ) clearInterval(timeoutId);
+    if( time <= 0 ) {
+      clearInterval(timeoutId);
+      timerRunning = false; // change running state
+    }
     updateTime(time, timerDisplay);
     console.log(time);
   }, 1000)
@@ -21,12 +27,14 @@ $(document).ready(function() {
 
   // time decrement handler
   $('span.minus-sign').click(function(){
+    if (timerRunning) return; // do nothing
     var node = $(this).next();
     node.text(parseInt(node.text(), 10) - 1);
   });
 
   // time increment handler
   $('span.plus-sign').click(function(){
+    if (timerRunning) return; // do nothing
     var node = $(this).prev();
     node.text(parseInt(node.text(), 10) + 1);
   });
