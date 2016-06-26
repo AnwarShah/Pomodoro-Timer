f0931e8cd20f2459ca0b1e0d26c2e35f79a4554f
Mohammad Anwar Shah <mohammadanwarshah@gmail.com>
2016-06-27 00:37:17 +0600

    update timer to use more robust object literal, implemented pause when clicked on timer display while running

--- js/script.js
+++ js/script.js
@@ -1,22 +1,29 @@
-var timerRunning = false; // timer state
-
-function updateTime(time, timerDisplay) {
-  timerDisplay.text(time);
-}
-
-function countDown(time, timerDisplay) {
-  var time = time;
-  timerRunning = true; // set the flag
-  var timeoutId = setInterval(function(){
-    time = time - 1;
-    if( time <= 0 ) {
-      clearInterval(timeoutId);
-      timerRunning = false; // change running state
-    }
-    updateTime(time, timerDisplay);
-    console.log(time);
-  }, 1000)
-}
+var timer = {
+  timerRunning: false,
+  timeoutId: null,
+  timerDisplay: null,
+  setTimer: function(time, timerDisplay){
+    this.timerRunning = true;
+    this.timerDisplay = timerDisplay;
+    this.timeoutId = setInterval(function(){
+      time = time - 1;
+      if( time <= 0 ) {
+        timer.stopTimer();
+      }
+      timer.displayTime(time);
+    }, 1000)
+  },
+  stopTimer: function() {
+    clearInterval(this.timeoutId);
+    this.timerRunning = false;
+  },
+  isRunning: function(){
+    return this.timerRunning;
+  },
+  displayTime: function(time) {
+    this.timerDisplay.text(time);
+  }
+};
 
 function readTime() {
   return $('#pomodoro-timer').find('span.time').text();
@@ -27,20 +34,24 @@ $(document).ready(function() {
 
   // time decrement handler
   $('span.minus-sign').click(function(){
-    if (timerRunning) return; // do nothing
+    if (timer.isRunning()) return; // do nothing
     var node = $(this).next();
     node.text(parseInt(node.text(), 10) - 1);
   });
 
   // time increment handler
   $('span.plus-sign').click(function(){
-    if (timerRunning) return; // do nothing
+    if (timer.isRunning()) return; // do nothing
     var node = $(this).prev();
     node.text(parseInt(node.text(), 10) + 1);
   });
 
   $('#time-display').click(function() {
-    time = readTime();
-    countDown(time, timerDisplay);
+    if (timer.isRunning()) {
+      timer.stopTimer()
+    } else {
+      time = readTime();
+      timer.setTimer(time, timerDisplay);
+    }
   });
 });
